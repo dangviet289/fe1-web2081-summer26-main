bd4d2456309b49c72cc77c6f653b18d889d4f4a9
dangviet289 <151553116+dangviet289@users.noreply.github.com>
2026-07-01 20:08:12 +0700

    bài lab2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
-import { About } from './about/about'; 
-import { Home } from './home/home';  
+import { About } from './pages/about/about'; 
+import { Home } from './pages/home/home';  
+import { Contact } from './pages/contact/contact';
+import { Products } from './pages/products/products';
 
 export const routes: Routes = [
   { path: '', component: Home },         
   { path: 'about', component: About },   
+  { path: 'contact', component: Contact },
+  { path: 'products', component: Products }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
+  id: number = 123;
   title = 'my-app';//property
   name = 'Bùi Đăng Việt';
   age = 22;
