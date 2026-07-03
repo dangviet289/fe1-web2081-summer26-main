aa8f2bef5e516e42288949dc5eeb35e6a7853d2b
dangviet289 <151553116+dangviet289@users.noreply.github.com>
2026-07-03 19:41:43 +0700

    bai lab3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { About } from './pages/about/about';
 import { Home } from './pages/home/home';  
 import { Contact } from './pages/contact/contact';
 import { Products } from './pages/products/products';
+import { Stories } from './pages/stories/stories';
 
 export const routes: Routes = [
   { path: '', component: Home },         
   { path: 'about', component: About },   
   { path: 'contact', component: Contact },
-  { path: 'products', component: Products }
+  { path: 'products', component: Products },
+  { path: 'stories', component: Stories }
 ];
